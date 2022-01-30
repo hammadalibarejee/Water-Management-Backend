385b550ae11ed1c247c3b0e4fcae1c0750d01555
Hammad Ali <34187587+hammadalibarejee@users.noreply.github.com>
2022-01-31 01:41:52 +0500

    Fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app= require('./app');
 require('dotenv').config('config.env');
 
-const PORT=3000 || process.env.PORT;
+const PORT=process.env.PORT || 3000;
 
 const server=app.listen(PORT,()=>{
     console.log(`The app is listening on port ${PORT}`);
